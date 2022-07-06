81fac51d06cacf94203b580317ec5f27ed97e73f
abhishek.pandey11 <abhishek.pandey@inaraconsultancy.com>
2022-07-06 20:07:20 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem("username");
+    if (user) {
+      navigate("/");
+    }
+  }, []);
 
   const handleLogin = () => {
     localStorage.setItem("username", username);
@@ -13,7 +21,6 @@ const Login = () => {
   return (
     <div className="h-[50vh] flex justify-center items-center">
       <section className="max-w-7xl mx-auto">
-
         <div className="text-2xl font-semibold uppercase my-5">Login here</div>
         <form
           action=""
@@ -22,7 +29,7 @@ const Login = () => {
         >
           <input
             type="text"
-			placeholder="Enter username"
+            placeholder="Enter username"
             onChange={(e) => setUsername(e.target.value)}
             className="rounded shadow-md my-2 border border-gray-500 px-10 py-2"
           />
--- src/pages/SingleUser.js
+++ src/pages/SingleUser.js
@@ -14,7 +14,6 @@ const SingleUser = () => {
     try {
       setIsLoading(true);
       const res = await axios.get(url);
-      console.log("res.data", res.data);
       setUser(res.data);
       setIsLoading(false);
     } catch (error) {
@@ -36,24 +35,24 @@ const SingleUser = () => {
         )}
       </div>
       {user && (
-        <section class="text-gray-600 body-font overflow-hidden">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="lg:w-4/5 mx-auto flex flex-wrap">
+        <section className="text-gray-600 body-font overflow-hidden">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap">
               <img
                 alt="ecommerce"
-                class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
                 src="https://dummyimage.com/400x400"
               />
-              <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 class="text-sm title-font text-gray-500 tracking-widest">
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">
                   {user.username}
                 </h2>
-                <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                   {user.name}
                 </h1>
-                <div class="flex mb-4">Email : {user.email}</div>
-                <p class="leading-relaxed">Phone : {user.phone}</p>
-                <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                <div className="flex mb-4">Email : {user.email}</div>
+                <p className="leading-relaxed">Phone : {user.phone}</p>
+                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                   <a href={`https://${user.website}`} target="_blank">
                     Website : {user.website}
                   </a>
